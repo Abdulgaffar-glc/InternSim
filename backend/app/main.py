0ee3be03c28fec2f44544c7e3e5637d1c7f09166
Abdülselam_İsmail <147073125+AbdulselamIsmail@users.noreply.github.com>
2026-01-19 11:50:06 +0300

    PostgreSQL and SQLAlchemy connection setup

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from .db_dep import get_db
 
 app = FastAPI(title="Intern Sim API")
 
 @app.get("/")
 def root():
     return {"status": "Intern Sim Backend Running"}
+
+@app.get("/db-test")
+def db_test(db: Session = Depends(get_db)):
+    return {"db": "connected"}
